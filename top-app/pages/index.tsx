fb72bb2081d38dda104795ad0f6c341c379b7699
Mike Timashov <bimbarr777@gmail.com>
2023-03-03 18:32:15 +0300

    use State

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Htag, Paragraph, Tag } from '@/components';
 
 
 export default function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
   return (
     <>
-      <Htag tag="h1">Текст</Htag>
-      <Button appearance='primary' arrow='right'>Кнопка</Button>
-      <Button appearance='ghost' arrow='right'>Кнопка</Button>
-      <Paragraph size='B'>Большой</Paragraph>
+      <Htag tag="h1">{counter}</Htag>
+      <Button appearance="primary" arrow="right" onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
+      <Button appearance="ghost" arrow="right" onClick={() => setCounter(x => x - 1)}>Кнопка</Button>
+      <Paragraph size="B">Большой</Paragraph>
       <Paragraph>Средний</Paragraph>
-      <Paragraph size='S'>Малекий</Paragraph>
-      <Tag size='S'>Ghost</Tag>
-      <Tag size='M' color='red'>Red</Tag>
-      <Tag size='S' color='green'>Green</Tag>
-      <Tag color='primary'>Primary</Tag>
+      <Paragraph size="S">Малекий</Paragraph>
+      <Tag size="S">Ghost</Tag>
+      <Tag size="M" color="red">Red</Tag>
+      <Tag size="S" color="green">Green</Tag>
+      <Tag color="primary">Primary</Tag>
 
     </>
   );

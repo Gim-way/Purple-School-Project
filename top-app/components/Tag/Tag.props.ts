b853ba9051e0ab8115bb12ad5811de36cb3634a8
Mikhail Timashov <96630329+Gim-way@users.noreply.github.com>
2023-03-05 22:07:05 +0300

    Delete top-app directory

--- top-app/components/Tag/Tag.props.ts
+++
@@ -1,9 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
-
-
-export interface TagProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  children: ReactNode;
-  size?: 'S' | 'M';
-  color?: 'ghost' | 'red' | 'grey' | 'green' | 'primary'
-  href?: string;
-}

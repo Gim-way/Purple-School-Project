7455f234d465708711735c46246a50bbc623aa3b
Mike Timashov <bimbarr777@gmail.com>
2023-03-12 21:37:38 +0300

    Product page layout - 1

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,15 +1,29 @@
 import { TopPageComponentProps } from '@/page-components/TopPageComponent/TopPageComponent.props'
+import { HhData, Htag, Tag } from '@/components'
+import styles from './TopPageComponent.module.css'
+import { TopLevelCategory } from '@/interfaces/page.interface'
 
 
 export const TopPageComponent = ({
   firstCategory,
   page,
   products,
-  ...props
 }: TopPageComponentProps): JSX.Element => {
   return (
-    <>
-      {products && products.length}
-    </>
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && <Tag color="grey" size="M">{products.length}</Tag>}
+        <span>Сортировка</span>
+      </div>
+      <div>
+        {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+      </div>
+      <div className={styles.hhTitle}>
+        <Htag tag="h2">Вакансии - {page.category}</Htag>
+        <Tag color="red" size="M">hh.ru</Tag>
+      </div>
+      { firstCategory == TopLevelCategory.Courses && <HhData {...page.hh}/>}
+    </div>
   )
 }

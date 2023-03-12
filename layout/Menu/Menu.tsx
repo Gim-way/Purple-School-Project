c39dba53511727482f4f14c529728f0c4e1085a0
Mike Timashov <bimbarr777@gmail.com>
2023-03-12 16:34:33 +0300

    useRouter

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -9,6 +9,7 @@ import { TopLevelCategory } from '@/interfaces/page.interface'
 import cn from 'classnames'
 import styles from './Menu.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 
 const firstLevelMenu: FirstLevelMenuItem[] = [
@@ -44,7 +45,16 @@ export const Menu = (): JSX.Element => {
     setMenu,
     firstCategory
   } = useContext(AppContext)
+  const router = useRouter()
 
+  const openSecondLevel = (secondCategory: string) => {
+    setMenu && setMenu(menu.map(m => {
+        if (m._id.secondCategory == secondCategory) {
+          m.isOpened = !m.isOpened
+        }
+        return m
+      }))
+  }
   const buldFirstLevel = () => {
     return (
       <>
@@ -67,18 +77,23 @@ export const Menu = (): JSX.Element => {
   const buldSecondLevel = (menuItem: FirstLevelMenuItem) => {
     return (
       <div className={styles.secondBlock}>
-        {menu.map(m => (
-          <div key={m._id.secondCategory}>
-            <div className={styles.secondLevel}>
-              {m._id.secondCategory}
-            </div>
-            <div className={cn(styles.secondLevelBlock, {
-              [styles.secondLevelBlockOpened]: m.isOpened
-            })}>
-              {buldThirdLevel(m.pages, menuItem.route)}
-            </div>
-          </div>
-        ))}
+        {menu.map(m => {
+          if (m.pages.map(p => p.alias).includes(router.asPath.split('/')[2])) {
+              m.isOpened = true
+          }
+            return (
+              <div key={m._id.secondCategory}>
+                <div className={styles.secondLevel} onClick={() =>openSecondLevel(m._id.secondCategory)}>
+                  {m._id.secondCategory}
+                </div>
+                <div className={cn(styles.secondLevelBlock, {
+                  [styles.secondLevelBlockOpened]: m.isOpened
+                })}>
+                  {buldThirdLevel(m.pages, menuItem.route)}
+                </div>
+              </div>
+            )
+        })}
       </div>
     )
   }
@@ -87,7 +102,7 @@ export const Menu = (): JSX.Element => {
       pages.map(p => (
         <Link href={`/${route}/${p.alias}`} legacyBehavior>
           <a className={cn(styles.thirdLevel, {
-            [styles.thirdLevelActive]: false
+            [styles.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath
           })}>
             {p.category}
           </a>

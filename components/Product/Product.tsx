91addbc4b7ab95bc7448ba29d7b78fde9c89f9bb
Mike Timashov <bimbarr777@gmail.com>
2023-03-13 00:19:03 +0300

    Declension of words

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -3,7 +3,7 @@ import { Card } from '../Card/Card';
 import { Rating } from '@/components';
 import { Tag } from '../Tag/Tag';
 import { Button } from '@/components';
-import { priceRU } from '@/helpers/helpers';
+import { declOfNum, priceRU } from '@/helpers/helpers';
 import { Divider } from '@/components';
 import styles from './Product.module.css';
 
@@ -14,7 +14,9 @@ export const Product = ({
 }: ProductProps): JSX.Element => {
   return (
     <Card className={styles.product}>
-      <div className={styles.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title}/></div>
+      <div className={styles.logo}>
+        <img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
+      </div>
       <div className={styles.title}>{product.title}</div>
       <div className={styles.price}>
         {priceRU(product.price)}
@@ -29,10 +31,18 @@ export const Product = ({
                                                                      color="ghost">{c}</Tag>)}</div>
       <div className={styles.priceTitle}>цена</div>
       <div className={styles.creditTitle}>кредит</div>
-      <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+      <div className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
       <Divider className={styles.hr}/>
       <div className={styles.description}>{product.description}</div>
-      <div className={styles.feature}>фичи</div>
+      <div className={styles.feature}>
+        {product.characteristics.map(c => (
+          <div className={styles.characteristics} key={c.name}>
+            <span className={styles.characteristicsName}>{c.name}</span>
+            <span className={styles.characteristicsDots}></span>
+            <span className={styles.characteristicsValue}>{c.value}</span>
+          </div>
+        ))}
+      </div>
       <div className={styles.advBlock}>
         {product.advantages && <div className={styles.advantages}>
           <div className={styles.advTitle}>Преимущества</div>

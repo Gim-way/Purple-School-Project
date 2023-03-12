dd995274b393c2a8a896583c5488010f73884559
Mike Timashov <bimbarr777@gmail.com>
2023-03-12 22:57:39 +0300

    Product page layout - 2

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,5 +1,5 @@
 import { TopPageComponentProps } from '@/page-components/TopPageComponent/TopPageComponent.props'
-import { HhData, Htag, Tag } from '@/components'
+import { Advantages, HhData, Htag, Paragraph, Tag } from '@/components'
 import styles from './TopPageComponent.module.css'
 import { TopLevelCategory } from '@/interfaces/page.interface'
 
@@ -23,7 +23,17 @@ export const TopPageComponent = ({
         <Htag tag="h2">Вакансии - {page.category}</Htag>
         <Tag color="red" size="M">hh.ru</Tag>
       </div>
-      { firstCategory == TopLevelCategory.Courses && <HhData {...page.hh}/>}
+      {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh}/>}
+      {page.advantages && page.advantages.length > 0 && <>
+      <Htag tag='h2'>Преимущества</Htag>
+        <Advantages advantages={page.advantages}/>
+      </>
+      }
+      {page.seoText && <Paragraph>{page.seoText}</Paragraph>}
+      <Htag tag='h2'>Получаемые навыки</Htag>
+      {page.tags.map(t => (
+        <Tag key={t} color='primary'>{t}</Tag>
+      ))}
     </div>
   )
 }
